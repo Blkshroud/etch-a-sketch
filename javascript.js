9d5ec2dd4c3b8d9b1f4eebeb45b52cb4e16cecf5
Blkshroud <145509002+Blkshroud@users.noreply.github.com>
2023-11-04 10:33:54 -0400

    Color choice kept when resetting or resizing now Grayscale commented out until fixed.

--- javascript.js
+++ javascript.js
@@ -1,32 +1,37 @@
 const container = document.querySelector("#container");
 
 let divAmount = 16;
-
+let color = 'black';
 const resetBtn = document.querySelector(".reset");
 resetBtn.addEventListener("click", resetBox);
 
 const resizeBtn = document.querySelector(".resize");
 resizeBtn.addEventListener("click", resizeBox);
 
-function initializeBox(){
-    for(let i = 0;i< (divAmount*divAmount);i++){
-        let divBox = document.createElement('div');
-        divBox.classList.add("empty");
-        divBox.style.flex = (100/divAmount) + "%";
-        divBox.style.paddingTop = (100/divAmount) + "%";
-        container.appendChild(divBox);
-        divBox.addEventListener("mouseover", hoverBlack);
-        function hoverBlack(){
-            divBox.classList.remove("empty");
-            divBox.classList.add("filled");
-        }
-    }
-}
+const RGBBtn = document.querySelector(".rainbow");
+RGBBtn.addEventListener("click", RGB);
 
-function initializeRGBBox(){
-    while(container.firstChild){
-        container.removeChild(container.firstChild);
-    }
+const blackBtn = document.querySelector(".black");
+blackBtn.addEventListener("click", black);
+
+// const grayBtn = document.querySelector(".gray");
+// grayBtn.addEventListener("click", gray);
+
+function black(){
+    color = 'black';
+    resetBox();
+}
+function RGB(){
+    color = 'RGB';
+    resetBox();
+}
+// function gray(){
+//     color = 'gray';
+//     resetBox();
+// }
+// let grayIntensity = 0.1;
+
+function initializeBox(color){
     for(let i = 0;i< (divAmount*divAmount);i++){
         let divBox = document.createElement('div');
         divBox.classList.add("empty");
@@ -34,22 +39,37 @@ function initializeRGBBox(){
         divBox.style.paddingTop = (100/divAmount) + "%";
         container.appendChild(divBox);
         divBox.addEventListener("mouseover", hoverColor);
-        function hoverColor(){
-            divBox.classList.remove("empty");
-            const randomColor = Math.floor(Math.random()*16777215).toString(16);
-            divBox.style.backgroundColor = "#" + randomColor;
-        }
+        function hoverColor(){  
+          
+            if(color == 'black'){
+                divBox.classList.remove("empty");
+                divBox.classList.add("filled");
+            }
+            else if(color == 'RGB'){
+                divBox.classList.remove("empty");
+                const randomColor = Math.floor(Math.random()*16777215).toString(16);
+                divBox.style.backgroundColor = "#" + randomColor;
+            }
+            // else if(color == 'gray'){
+            //     divBox.classList.remove("empty");
+            //     grayIntensity += 0.1;
+            //     divBox.style.backgroundColor = `rgba(0,0,0,${grayIntensity})`;
+
+            // }
+            else{
+                console.log("You made an oopsie in your code!");
+            }
+        }        
     }
 }
 
-const RGBBtn = document.querySelector(".rainbow");
-RGBBtn.addEventListener("click", initializeRGBBox);
+
 
 function resetBox(){
     while(container.firstChild){
         container.removeChild(container.firstChild);
     }
-    initializeBox();
+    initializeBox(color);
 }
 
 function resizeBox(){
@@ -66,4 +86,4 @@ function resizeBox(){
 }
 
 
-initializeBox();
+initializeBox(color);
