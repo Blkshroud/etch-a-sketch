869fcde123c9c490e868bfdcda3e4aa711c7dbe4
Blkshroud <145509002+Blkshroud@users.noreply.github.com>
2023-11-03 21:41:08 -0400

    Added Reset button which uncolors sketchpad.

--- javascript.js
+++ javascript.js
@@ -1,9 +1,25 @@
 const container = document.querySelector("#container");
 let divAmount = 16;
+const resetBtn = document.querySelector(".reset");
+resetBtn.addEventListener("click", resetBox);
 
-for(let i = 0;i< (divAmount*divAmount);i++){
-    const divBox = document.createElement('div');
-    divBox.classList.add("empty");
-    container.appendChild(divBox);
+function initializeBox(){
+    for(let i = 0;i< (divAmount*divAmount);i++){
+        let divBox = document.createElement('div');
+        divBox.classList.add("empty");
+        container.appendChild(divBox);
+        divBox.addEventListener("mouseover", hoverColor);
+        function hoverColor(){
+            divBox.classList.remove("empty");
+            divBox.classList.add("filled");
+        }
+    }
+}
+function resetBox(){
+    while(container.firstChild){
+        container.removeChild(container.firstChild);
+    }
+    initializeBox();
 }
 
+initializeBox();
