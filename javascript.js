3730cc67343cf784c5746e568a4061b21981f795
Blkshroud <145509002+Blkshroud@users.noreply.github.com>
2023-11-03 21:55:06 -0400

    Refactored H & W into JS for easy changing by user Uses 100 / grid size to keep dimensions the same.

--- javascript.js
+++ javascript.js
@@ -7,6 +7,8 @@ function initializeBox(){
     for(let i = 0;i< (divAmount*divAmount);i++){
         let divBox = document.createElement('div');
         divBox.classList.add("empty");
+        divBox.style.flex = (100/divAmount) + "%";
+        divBox.style.paddingTop = (100/divAmount) + "%";
         container.appendChild(divBox);
         divBox.addEventListener("mouseover", hoverColor);
         function hoverColor(){
@@ -20,6 +22,10 @@ function resetBox(){
         container.removeChild(container.firstChild);
     }
     initializeBox();
+}
+
+function getNewSize(){
+
 }
 
 initializeBox();
